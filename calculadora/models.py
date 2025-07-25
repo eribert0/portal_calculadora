61f3ddff4a463d36b609d091a03b8395cf2ead05
Eriberto Junior <43413666+eribert0@users.noreply.github.com>
2025-07-25 07:26:36 -0300

    feat: Define modelo de dados para Operacao
    Cria o modelo 'Operacao' conforme especificado nos requisitos do desafio.

--- calculadora/models.py
+++ calculadora/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Operacao(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    parametros = models.CharField(max_length=100)
+    resultado = models.CharField(max_length=60)
+    data_inclusao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Operação de {self.usuario.username}: {self.parametros} = {self.resultados}'
